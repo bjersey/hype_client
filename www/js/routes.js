19333f2927684992aa66abba090e68df2748b82e
brian <brian.lao@gmail.com>
2016-02-24 19:53:01 -0500

    fix default route

--- www/js/routes.js
+++ www/js/routes.js
@@ -21,5 +21,5 @@ angular.module('hype_client').config(function ($stateProvider, $urlRouterProvide
       controller: 'ProfileController'
     });
 
-  $urlRouterProvider.otherwise('/heatmap');
+  $urlRouterProvider.otherwise('heatmap');
 });
