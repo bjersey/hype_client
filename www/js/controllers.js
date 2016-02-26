869de5f6b6629cdf9f5ae127e05640feb6cac3cf
brian <brian.lao@gmail.com>
2016-02-26 18:12:29 -0500

    add advertisement bar

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -66,7 +66,7 @@ angular.module('hype_client').controller('HeatMapController', function ($scope, 
     $scope.calcRegionPosition = function calcRegionPosition(idx) {
 
       var foo = (idx % 3) * 33;
-      var bar = _.floor(idx / 3) * 50;
+      var bar = _.floor(idx / 3) * 45;
 
       var top = bar + '%';
       var left = foo + '%';
